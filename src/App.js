0712b2c751d7051a83d9d64123e8b19dc7d049b3
raihan-778 <raihanarif778@gmail.com>
2022-10-07 07:52:10 +0600

    react-router-added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import './App.css';
-import Header from './components/header/Header';
-import "./components/header/Header.css";
-import Shop from './components/shop/Shop';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import About from "./components/About/About";
+import Inventory from "./components/Inventory/Inventory";
+import Orders from "./components/Orders/Orders";
+import Shop from "./components/shop/Shop";
+import Main from "./Layouts/Main/Main";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Shop></Shop>,
+        },
+        {
+          path: "/orders",
+          element: <Orders></Orders>,
+        },
+        {
+          path: "/inventory",
+          element: <Inventory />,
+        },
+        {
+          path: "about",
+          element: <About></About>,
+        },
+      ],
+    },
+  ]);
+
   return (
-    <div >
-      <Header></Header>
-      <Shop></Shop>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../images/Logo.svg";
+import "./Header.css";
 
 const Header = () => {
   return (
     <nav className="header">
       <img src={logo} alt="logo" />
       <div className="container">
-        <a href="/Home">Home</a>
-        <a href="/Shop">Shop</a>
+        {/* <a href="/">Home</a>
+        <a href="/">Shop</a>
         <a href="/About">About</a>
-        <a href="/Order">Order</a>
-        <a href="/Enventory">Enventory</a>
+        <a href="/orders">Order</a>
+        <a href="/inventory">Inventory</a> */}
+        <Link to={`/`}>Home</Link>
+        <Link to={`/`}>Shop</Link>
+        <Link to={`/orders`}>Orders</Link>
+        <Link to={`/inventory`}>Inventory</Link>
+        <Link to={`/about`}>About</Link>
       </div>
     </nav>
   );
